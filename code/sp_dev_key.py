c4e811f89326b6a3364750b65cad7997d4ab2029
emrig <emrig@users.noreply.github.com>
2018-02-27 14:46:33 -0500

    Initial release of Sptoify Artist Search, no tests
    Initial release to start developing tests.

---
+++ code/sp_dev_key.py
@@ -0,0 +1,8 @@
+#Including for test purposes, these will not be prod keys
+
+client_id = 'ce1d14920acb4c569fe9f5e7d03fe4f2'
+client_secret = 'b7640cb85558436d9d030e6d42b11d6a'
+
+SPOTIFY_CLIENT_ID='ce1d14920acb4c569fe9f5e7d03fe4f2'
+SPOTIFY_CLIENT_SECRET='b7640cb85558436d9d030e6d42b11d6a'
+SPOTIFY_REDIRECT_URI='http://localhost:8888/callback'
---
+++ code/sp_search.py
@@ -0,0 +1,37 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from sp_dev_key import SPOTIFY_CLIENT_ID
+from sp_dev_key import SPOTIFY_CLIENT_SECRET
+
+"""
+Refer to https://spotipy.readthedocs.io/en/latest/#api-reference 
+for Spotipy API reference
+"""
+
+class sp_search:
+    def __init__(self):
+
+        CLIENT_ID = SPOTIFY_CLIENT_ID
+        CLIENT_SECRET = SPOTIFY_CLIENT_SECRET
+
+        self.client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
+
+    def artist(self, artist_name, limit=None):
+
+        spotify = spotipy.Spotify(client_credentials_manager=self.client_credentials_manager)
+        result = spotify.search(q='artist:' + artist_name, limit=limit, type='Artist')
+
+
+        return result
+
+if __name__ == '__main__':
+
+    # Use this for local testing, reference for how this works
+    model = sp_search()
+    results = model.artist("Michael")
+
+    results_list = results['artists']['items']
+
+    for result in results_list:
+
+        print((result['name'], result['id'], result['popularity']))
