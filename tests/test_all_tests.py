97ad6e0c6c42eb0942dfc3f4d59caa3593922c34
emrig <emrig@users.noreply.github.com>
2018-03-08 20:20:48 -0500

    Add Test Suite Main, Fix OS Path

--- tests/test_all_tests.py
+++ tests/test_all_tests.py
@@ -12,7 +12,7 @@ directory = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pard
 print(directory)
 
 """Enter the names of modules in tests folder to omit"""
-no_include = open(directory + '\\' + ignore_file, 'r').read().splitlines()
+no_include = open(os.path.join(directory, ignore_file), 'r').read().splitlines()
 
 modules = [x.strip('.py') for x in os.listdir(os.path.join(os.path.dirname(__file__), os.path.pardir, 'tests'))
               if x.endswith('.py') and x not in no_include]
