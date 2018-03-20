55630987f9642ceb7800c114c94510643c5a6b1e
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-03-20 16:29:51 -0400

    completed tracks but error [CI Skip]

--- backend/album_crawler.py
+++ backend/album_crawler.py
@@ -17,6 +17,8 @@ class album_crawler():
         self.firebase = pyrebase.initialize_app(pyrebaseConfig)
         self.database = self.firebase.database()
 
+        self.trackConfig = envData.config['databaseConfig']['track_features']['track_q']['track_id']
+
         self.search = sp_search.sp_search()
 
         self.read_input()
@@ -30,13 +32,12 @@ class album_crawler():
 
         # find max datetime in get_music_time
         # store all artists who weren't updated fully previous time/were never updated
+        # and store music for all these artists
         nullArtists = self.database.child("crawlerTable").child("artist_q").order_by_child("get_music_time").equal_to("").get()
-        if lastUpdated.pyres:
-            notUpdatedArtists = self.database.child("crawlerTable").child("artist_q").order_by_child("get_music_time").start_at(lastUpdatedTime).get()
-
-        # store music for all these artists
         self.findTracks(nullArtists)
-        self.findTracks(notUpdatedArtists)
+        if lastUpdatedTime != '':
+            notUpdatedArtists = self.database.child("crawlerTable").child("artist_q").order_by_child("get_music_time").start_at(lastUpdatedTime).get()
+            self.findTracks(notUpdatedArtists)
 
     def findTracks(self, toBeUpdated):
 
@@ -48,8 +49,8 @@ class album_crawler():
             #TODO: figure how to keep track of albums when Spotify deletes album and adds another
             if (albums["total"] > artist["albums"]):
                 # find missing album(s)
-                missingAlbums = self.findMissingAlbums(artist, albums)
-                updateCount = self.updateTracks(missingAlbums)
+                missingAlbums = self.findMissingAlbums(item.key(), albums)
+                updateCount = self.updateTracks(item.key(), missingAlbums)
                 # update number of albums
                 artist["albums"] = albums["total"] + updateCount
             # keep track of all successfully traversed artists
@@ -67,16 +68,19 @@ class album_crawler():
     def findMissingAlbums(self, artist, albums):
         missingAlbums = []
 
-        #TODO: retrieve list of 'artist's album names from DB (csv)
+        # retrieve list of 'artist's album names from DB (csv)
         #TODO: handle if artist is new to DB (csv) - returns null existingAlbums
-        existingAlbums = []
+        # existsInDB = self.database.child("track_features").child("track_q").order_by_child("artist_id").equal_to(artist).get()
+        # existingAlbums = existsInDB.val()
+
         for album in albums["items"]:
-            if (album["name"] not in existingAlbums):
-                missingAlbums.append(album["id"])
+            #TODO: remove below comment after initial DB population
+            # if (album["id"] not in existingAlbums):
+            missingAlbums.append(album["id"])
 
         return missingAlbums
 
-    def updateTracks(self, missingAlbums):
+    def updateTracks(self, artist, missingAlbums):
         updateCount = 0
 
         #TODO: place in try-catch for API limit error - run by Album - Find the error that spotipy raises!
@@ -86,14 +90,29 @@ class album_crawler():
             tracks = self.search.album_tracks(album)
             # query for audio features
             for track in tracks["items"]:
-                #TODO: write relevant from both data to DB (csv)
-                trackId = track["id"]
-                features = self.search.audio_features(trackId)
-                # write to DB (csv) here - also write album name
+                # write relevant data to DB (csv)
+                entry = self.initializeEntry(track)
+                entry["artist_id"] = artist
+                entry["album_id"] = album["id"]
+                self.database.child('track_features').child('track_q').update({track["id"]: entry})
             # increment only if entire album update is successful
             updateCount += 1
 
         return updateCount
+
+    def initializeEntry(self, track):
+        trackId = track["id"]
+        features = self.search.audio_features(trackId)
+
+        entry = self.trackConfig
+        entry["name"] = track['name']
+        entry["danceability"] = features["danceability"]
+        entry["key"] = features["key"]
+        entry["valence"] = features["valence"]
+        entry["tempo"] = features["tempo"]
+        entry["time_signature"] = features["time_signature"]
+
+        return entry
 
 if __name__ == "__main__":
     crawler = album_crawler()
