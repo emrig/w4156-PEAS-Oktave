124dd59df35f2e562db3249765bc0ef1a9bbc1c5
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-03-27 19:24:25 -0400

    some tracks added

--- backend/album_crawler.py
+++ backend/album_crawler.py
@@ -2,10 +2,14 @@
 Author: Pankhuri Kumar
 """
 
-import pyrebase
 import sp_search
 import environment
 from datetime import datetime
+import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from google.cloud.exceptions import NotFound
 
 class album_crawler():
 
@@ -14,69 +18,88 @@ class album_crawler():
         envData = environment.Data()
 
         pyrebaseConfig = envData.config['pyrebaseConfig']
-        self.firebase = pyrebase.initialize_app(pyrebaseConfig)
-        self.database = self.firebase.database()
+        localCredentials = credentials.Certificate(pyrebaseConfig['serviceAccount'])
+        firebase_admin.initialize_app(localCredentials)
+        self.database = firestore.client()
+        self.artistReference = self.database.collection(u'artist_q')
+        self.trackReference = self.database.collection(u'track_q')
 
-        self.trackConfig = envData.config['databaseConfig']['track_features']['track_q']['track_id']
-
+        self.trackConfig = envData.config['databaseConfig']['track_q']['track_id']
         self.search = sp_search.sp_search()
 
         self.read_input()
 
     def read_input(self):
 
-        lastUpdatedTime = ""
-        lastUpdated = self.database.child("crawlerTable").child("artist_q").order_by_child("get_music_time").limit_to_last(1).get().val()
+        lastUpdatedTime = ''
+        lastUpdated = self.artistReference.order_by(u'get_music_time', direction=firestore.Query.DESCENDING).limit(1).get()
         for a in lastUpdated:
-            lastUpdatedTime = lastUpdated[a]["get_music_time"]
+            lastUpdatedTime = (a.to_dict())["get_music_time"]
+
+        # update artists_queue with get_music_time and new album count
+        s = datetime.now()
+        self.updateTime = time.mktime(s.timetuple())
 
         # find max datetime in get_music_time
         # store all artists who weren't updated fully previous time/were never updated
         # and store music for all these artists
-        nullArtists = self.database.child("crawlerTable").child("artist_q").order_by_child("get_music_time").equal_to("").get()
-        self.findTracks(nullArtists)
-        if lastUpdatedTime != '':
-            notUpdatedArtists = self.database.child("crawlerTable").child("artist_q").order_by_child("get_music_time").start_at(lastUpdatedTime).get()
-            self.findTracks(notUpdatedArtists)
+        found = 1
+        while found != 0:
+            try:
+                nullArtist = self.artistReference.where(u'get_music_time', u'==', 0).order_by(u'index').limit(1).get()
+                self.findTracks(nullArtist)
+                print(found)
+                found += 1
+            except NotFound:
+                found = 0
+                pass
+
+        if lastUpdatedTime != 0:
+            found1 = 1
+            while found1 != 0:
+                try:
+                    notUpdatedArtist = self.artistReference.where(u'get_music_time', u'<', lastUpdatedTime).order_by(u'index').limit(1).get()
+                    self.findTracks(notUpdatedArtist)
+                    print(found1)
+                    found1 += 1
+                except NotFound:
+                    found1  = 0
+                    pass
 
     def findTracks(self, toBeUpdated):
 
-        updatedArtists = []
-
-        for item in toBeUpdated.each():
-            albums = self.search.artist_albums(item.key())
-            artist = item.val()
+        for item in toBeUpdated:
+            print("Starting Artist: " + str(item.id))
+            albums = self.search.artist_albums(item.id)
+            artist = item.to_dict()
             #TODO: figure how to keep track of albums when Spotify deletes album and adds another
             if (albums["total"] > artist["albums"]):
                 # find missing album(s)
-                missingAlbums = self.findMissingAlbums(item.key(), albums)
-                updateCount = self.updateTracks(item.key(), missingAlbums)
+                missingAlbums = self.findMissingAlbums(item.id, albums)
+                updateCount = self.updateTracks(item.id, missingAlbums)
                 # update number of albums
                 artist["albums"] = albums["total"] + updateCount
-            # keep track of all successfully traversed artists
-            updatedArtists.append(item.key())
-
-        # update artists_queue with get_music_time and new album count
-        updateTime = datetime.now()
-        for item in toBeUpdated.each():
-            artist = item.val()
-            if item.key() in updatedArtists:
-                artist["get_music_time"] = updateTime
+                artist["get_music_time"] = self.updateTime
                 # write updates to DB (csv)
-                self.database.child("crawlerTable").child("artist_q").update({item.key(): artist})
+                self.artistReference.document(item.id).update(artist)
+                print("Done with " + str(item.id))
 
     def findMissingAlbums(self, artist, albums):
         missingAlbums = []
 
         # retrieve list of 'artist's album names from DB (csv)
         #TODO: handle if artist is new to DB (csv) - returns null existingAlbums
-        # existsInDB = self.database.child("track_features").child("track_q").order_by_child("artist_id").equal_to(artist).get()
-        # existingAlbums = existsInDB.val()
+        # existsInDB = self.trackReference.where(u'artist_id', u'==', artist).get()
+        # existingAlbums = []
+        # for e in existsInDB:
+        #     existingAlbums.append(e.id)
 
         for album in albums["items"]:
             #TODO: remove below comment after initial DB population
             # if (album["id"] not in existingAlbums):
             missingAlbums.append(album["id"])
+            if len(missingAlbums) > 5:
+                break
 
         return missingAlbums
 
@@ -93,26 +116,40 @@ class album_crawler():
                 # write relevant data to DB (csv)
                 entry = self.initializeEntry(track)
                 entry["artist_id"] = artist
-                entry["album_id"] = album["id"]
-                self.database.child('track_features').child('track_q').update({track["id"]: entry})
+                entry["album_id"] = album
+                if not self.exists(track['id']):
+                    self.trackReference.document(track['id']).set(entry)
             # increment only if entire album update is successful
             updateCount += 1
+            print("Updated " + str(album))
 
         return updateCount
 
     def initializeEntry(self, track):
         trackId = track["id"]
-        features = self.search.audio_features(trackId)
+        # print(features[0])
 
         entry = self.trackConfig
         entry["name"] = track['name']
-        entry["danceability"] = features["danceability"]
-        entry["key"] = features["key"]
-        entry["valence"] = features["valence"]
-        entry["tempo"] = features["tempo"]
-        entry["time_signature"] = features["time_signature"]
-
+        try:
+            features = self.search.audio_features(str(trackId))
+            entry["danceability"] = features[0]["danceability"]
+            entry["key"] = features[0]["key"]
+            entry["valence"] = features[0]["valence"]
+            entry["tempo"] = features[0]["tempo"]
+            entry["time_signature"] = features[0]["time_signature"]
+        except:
+            pass
         return entry
+
+    def exists(self, trackID):
+        trackRef = self.trackReference.document(trackID)
+
+        try:
+            doc = trackRef.get()
+            return True
+        except NotFound:
+            return False
 
 if __name__ == "__main__":
     crawler = album_crawler()
