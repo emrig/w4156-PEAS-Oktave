69da102efd2fb5320dd6f9b18178114676afbd45
Sam <sam.m.moghadam@gmail.com>
2018-04-08 16:56:12 -0400

    [skip ci]

--- static/genre_search.js
+++ static/genre_search.js
@@ -108,12 +108,12 @@ $(document).ready(function() {
 		//$("#search-results").show();
 		//$("#search-results").hide();
 
-		var obj = $.get("127.0.0.1:5000", parse(obj));
+		var data = $.get("127.0.0.1:5000", parse(data));
 	});
 
 	function parse(data)
 	{
-		console.log(obj.data[0]);
+		console.log("HELLO WORLD");
 	}
 
 
