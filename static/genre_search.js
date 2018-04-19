0c79261e72fd4c19549d68d85078159584e23d73
Sam <sam.m.moghadam@gmail.com>
2018-04-18 20:05:09 -0400

    [skip ci]

--- static/genre_search.js
+++ static/genre_search.js
@@ -139,7 +139,8 @@ $(document).ready(function() {
 		// create the grid passing in the div to use together with the columns & data we want to use
 		new agGrid.Grid(eGridDiv, gridOptions);
 
-		gridOptions.api.setRowData(json["data"]);
+		console.log(json["data"]);
+		//gridOptions.api.setRowData(json["data"]);
 	});
 
 	function parse(json)
