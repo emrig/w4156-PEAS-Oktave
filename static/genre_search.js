e1293bfbca7fb5db0b79edab91e2ed199a7ba720
Sam <sam.m.moghadam@gmail.com>
2018-04-22 16:36:21 -0400

    [skip ci]

--- static/genre_search.js
+++ static/genre_search.js
@@ -1,88 +1,11 @@
 
 $(document).ready(function() {
 
-	var genres = [
-	'adult standards',
-	 'afrobeat',
-	 'album rock',
-	 'art rock',
-	 'blues',
-	 'blues-rock',
-	 'brill building pop',
-	 'chicago soul',
-	 'christmas',
-	 'classic funk rock',
-	 'classic rock',
-	 'country rock',
-	 'dance pop',
-	 'dance rock',
-	 'deep funk',
-	 'delta blues',
-	 'disco',
-	 'electric blues',
-	 'folk rock',
-	 'folk',
-	 'funk',
-	 'glam rock',
-	 'hard rock',
-	 'heavy christmas',
-	 'hip pop',
-	 'jazz blues',
-	 'jazz christmas',
-	 'jazz funk',
-	 'lounge',
-	 'melancholia',
-	 'mellow gold',
-	 'memphis blues',
-	 'memphis soul',
-	 'modern blues',
-	 'motown',
-	 'neo soul',
-	 'new jack swing',
-	 'new orleans blues',
-	 'new romantic',
-	 'new wave pop',
-	 'new wave',
-	 'northern soul',
-	 'p funk',
-	 'pop',
-	 'post-disco',
-	 'post-punk',
-	 'protopunk',
-	 'psychedelic rock',
-	 'quiet storm',
-	 'r&b',
-	 'rock',
-	 'rock-and-roll',
-	 'rockabilly',
-	 'roots rock',
-	 'singer-songwriter',
-	 'smooth jazz',
-	 'soft rock',
-	 'soul blues',
-	 'soul christmas',
-	 'soul jazz',
-	 'soul',
-	 'southern rock',
-	 'southern soul',
-	 'symphonic rock',
-	 'synthpop',
-	 'uk post-punk',
-	 'urban contemporary',
-	 'vocal jazz'
-	 ];
-	$( "#genre-search-bar" ).autocomplete({
-	    source: genres
-	});
-
-
 	$("#search-form-master").submit(function(event) {
 		document.getElementById("myGrid").remove();
 		var grid_element = $("<div id='myGrid' style='height: 600px;width:100%;' class='ag-theme-balham'>");
 		$("#search-results").append(grid_element);
 
-		//console.log("Button clicked");
-		//var genre = $("#genre-search-bar").val();
 		var tempo = $("#tempo-search-bar").val();
 		var key = $("#key-list").val();
 		var time_signature = $("#time-sig-list").val();
@@ -92,7 +15,6 @@ $(document).ready(function() {
 
 		// Create JSON object with property "genre_label"
 		var json_object = {
-			//genre: genre,   
 			tempo: tempo,
 			key: key,
 			time_signature: time_signature
@@ -127,7 +49,7 @@ $(document).ready(function() {
 		{headerName: "Loudness", field: "loudness"},
 		{headerName: "Speechiness", field: "speechiness"},
 		{headerName: "Happiness", field: "valence"},
-		{headerName: "Duration (ms)", field: "duration_ms"}
+		{headerName: "Length (ms)", field: "duration_ms"}
 
 	    ];
 
