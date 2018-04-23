973b26db6117726e1ec92cf57f3a90d3a1fe2e62
Sam <sam.m.moghadam@gmail.com>
2018-04-22 20:05:01 -0400

    [skip ci] Fixed several bugs in UI.

--- static/genre_search.js
+++ static/genre_search.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	$("#search-form-master").submit(function(event) {
 		document.getElementById("myGrid").remove();
-		var grid_element = $("<div id='myGrid' style='height: 600px;width:100%;' class='ag-theme-balham'>");
+		var grid_element = $("<div id='myGrid' style='height: 600px;width:auto;' class='ag-theme-balham'>");
 		$("#search-results").append(grid_element);
 
 		var tempo = $("#tempo-search-bar").val();
@@ -29,13 +29,98 @@ $(document).ready(function() {
 		var json = $.get("/attribute_search", json_object, function(json) {parse(json);});
 	});
 
-	function parse(json)
+	$("#song-search-form-master").submit(function(event) {
+		document.getElementById("myGrid").remove();
+		var grid_element = $("<div id='myGrid' class='ag-theme-balham'>");
+		$("#search-results").append(grid_element);
+
+		var track_name = $("#song-search-bar").val();
+
+		// Create JSON object with property "genre_label"
+		var json_object = {
+			track_name: track_name,
+		}
+
+		// Encode JSON string
+		var json_string = JSON.stringify(json_object);
+
+		var json = $.get("/track_search", json_object, function(json) {parse_song(json);});
+		//console.log(json);
+	});
+
+	function parse_song(json) 
 	{
 		// specify the columns
 		var columnDefs = [
 		{headerName: "Album Art", field: "album_art", cellRenderer: function(params) {
       return '<img src="'+ params.value + '" height="200" width="200">'
   }, autoHeight:true},
+		{headerName: "Song", field: "name"},
+		{headerName: "Artist Name", field: "artist_name"},
+		{headerName: "Album", field: "album_name"},
+		{headerName: "Song ID", field: "id", hide:true}
+		];
+
+		// let the grid know which columns and what data to use
+    	var gridOptions = {
+    		columnDefs: columnDefs,
+    		enableSorting: true,
+    		enableFilter: true,
+    		rowSelection: 'single',
+    		onSelectionChanged: onSelectionChanged
+    	};
+
+    	// lookup the container we want the Grid to use
+		var eGridDiv = document.querySelector('#myGrid');
+
+		// create the grid passing in the div to use together with the columns & data we want to use
+		new agGrid.Grid(eGridDiv, gridOptions);
+		gridOptions.api.setRowData(json["data"]);
+		gridOptions.rowHeight = 600;
+
+		var selectedRows = gridOptions.api.getSelectedRows();
+
+    	function onSelectionChanged() {
+    	    var selectedRows = gridOptions.api.getSelectedRows();
+    	    var selectedRowsString = '';
+    	    selectedRows.forEach( function(selectedRow, index) {
+    	        if (index!==0) {
+    	            selectedRowsString += ', ';
+    	        }
+    	        selectedRowsString += selectedRow.id;
+    	    });
+    	   
+    	   // Create JSON object with property "genre_label"
+    	   console.log(selectedRowsString);
+    	   var json_object = {
+    	   track_id: selectedRowsString
+    	}
+
+    		document.getElementById("myGrid").remove();
+			var grid_element = $("<div id='myGrid' class='ag-theme-balham' style='margin: auto; height: 600px; width: 100%;'>");
+			$("#search-results").append(grid_element);
+
+    	   // Encode JSON string
+    	   var json_string = JSON.stringify(json_object);
+
+    	   var json = $.get("/id_search", json_object, function(json) {parse(json);});
+		}
+	}
+
+
+	function parse(json)
+	{
+		// specify the columns
+		var columnDefs = [
+
+		{headerName: "Album Art", field: "album_art", cellRenderer: function(params) {
+      return '<img src="'+ params.value + '" height="200" width="200">'
+  }, autoHeight:true},
+  		
+  		{headerName: "Preview Song", field: "preview_url", cellRenderer: function(params) {
+      return '<audio id="player" controls="false" name="media"><source src="'+ params.value +'" type="audio/mpeg"></audio>'
+  }, autoHeight:true},
+
 		{headerName: "Song", field: "name"},
 		{headerName: "Artist Name", field: "artist_name"},
 		{headerName: "Tempo", field: "tempo"},
@@ -66,7 +151,7 @@ $(document).ready(function() {
 
 		// create the grid passing in the div to use together with the columns & data we want to use
 		new agGrid.Grid(eGridDiv, gridOptions);
-		gridOptions.api.setRowData(json["results"]);
+		gridOptions.api.setRowData(json["data"]["results"]);
 		gridOptions.rowHeight = 600;
 	}
 
